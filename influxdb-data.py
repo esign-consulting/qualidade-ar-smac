908250a41cbe7e96ab3b8b10143857741a504cf1
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2020-12-10 11:33:43 +0000

    smac port

--- influxdb-data.py
+++ influxdb-data.py
@@ -2,11 +2,24 @@
 
 import docker
 import requests
+import socket
 import time
 
-def healthy():
+def get_port(port=8080):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while port <= port + 10:
+        try:
+            sock.bind(('', port))
+            sock.close()
+            return port
+        except OSError:
+            port += 1
+    raise IOError('no free ports')
+
+def healthy(port):
     try:
-        r = requests.head("http://localhost:8080/actuator/health")
+        endpoint = "http://localhost:%s/actuator/health" % port
+        r = requests.head(endpoint)
         return r.status_code == 200
     except requests.exceptions.ConnectionError:
         return False
@@ -21,14 +34,16 @@ if not client.images.list(name=image):
 containers = client.containers.list(filters={"ancestor":image})
 if not containers:
     print("Running %s..." % image)
-    container = client.containers.run(image=image, name="smac", remove=True, detach=True, ports={"8080/tcp":"8080"})
+    port = get_port()
+    container = client.containers.run(image=image, name="smac", remove=True, detach=True, ports={"8080/tcp":port})
     stop = True
 else:
     container = containers[0]
+    port = int(container.attrs["NetworkSettings"]["Ports"]["8080/tcp"][0]["HostPort"])
     stop = False
 
 i = 1
-while not healthy():
+while not healthy(port):
     if i > 5:
         print("Max retries exceeded.")
         break

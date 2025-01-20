1c7d648cb0c017bb23159d964147ce3bc5c6b088
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2025-01-20 16:20:43 +0000

    influxdb remaining data dag fix

--- airflow/influxdb-remaining-data-dag.py
+++ airflow/influxdb-remaining-data-dag.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from airflow import DAG
 from airflow.decorators import task
 from airflow.utils.dates import days_ago
@@ -7,12 +5,17 @@ from airflow.utils.dates import days_ago
 from apiclient import BoletimRequestor
 from apiinfluxdb import InfluxDB
 
+import datetime
+import logging
+
 with DAG(dag_id="smac_etl", start_date=days_ago(1), schedule="0 0 * * *") as dag:
 
-    @task()
+    @task
     def influxdb_remaining_data():
-        requestor = BoletimRequestor()
-        influxdb = InfluxDB()
+        requestor = BoletimRequestor(url="http://smac:8080/smac")
+        influxdb = InfluxDB(url="http://influxdb:8086")
+
+        logging.info("Writing to influxDB...")
         last_date = influxdb.get_last_timestamp().date()
         next_date = last_date + datetime.timedelta(1)
         today = datetime.date.today()
@@ -21,4 +24,9 @@ with DAG(dag_id="smac_etl", start_date=days_ago(1), schedule="0 0 * * *") as dag
             boletim = requestor.request(d_string)
             if boletim and boletim.data == d_string:
                 influxdb.write_boletim(boletim)
+                logging.info(f"Data from {boletim.data} written.")
             next_date = next_date + datetime.timedelta(1)
+
+        logging.info("Done.")
+
+    influxdb_remaining_data()

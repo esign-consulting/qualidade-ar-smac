f294db8d8ab935476fbd4b47892a1cc4f08199f8
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2025-01-30 14:49:40 +0000

    replace influxdb with timescaledb

--- influxdb/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-influx write -b ${DOCKER_INFLUXDB_INIT_BUCKET_ID} -f /docker-entrypoint-initdb.d/data.line
-
-influx bucket create -n qualidadear-horaria
-influx write -b qualidadear-horaria -f /docker-entrypoint-initdb.d/historical-data.line

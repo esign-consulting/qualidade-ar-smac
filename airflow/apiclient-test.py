0f1560c061543448dc0c0c33ad52c4327c694b55
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2025-01-31 16:40:51 +0000

    pytest

--- airflow/apiclient-test.py
+++ airflow/apiclient-test.py
@@ -4,20 +4,18 @@ from apiclient import BoletimRequestor
 from apidocker import SMAC
 
 import datetime
-import logging
 
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(levelname)s - %(message)s")
+def test_apiclient():
+    smac = SMAC()
+    port = smac.run()
+    requestor = BoletimRequestor(f"http://localhost:{port}/smac")
 
-smac = SMAC()
-port = smac.run()
-requestor = BoletimRequestor(f"http://localhost:{port}/smac")
+    today = datetime.date.today()
+    d_string = today.strftime("%d/%m/%Y")
+    boletim = requestor.request(d_string)
+    smac.stop()
 
-today = datetime.date.today()
-d_string = today.strftime("%d/%m/%Y")
-boletim = requestor.request(d_string)
-if boletim:
-    for estacao in boletim.estacoes:
-        logging.info(f"{estacao.codigo} - {estacao.nome}")
-
-smac.stop()
+    assert boletim
+    assert len(boletim.estacoes) > 0
+    assert next((e for e in boletim.estacoes if e.nome == "Centro"), None)

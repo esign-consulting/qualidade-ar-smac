308c851f476bf1040ea95cf0479d48cc7ceca964
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2025-02-04 20:17:29 +0000

    estacoes table

---
+++ airflow/apitimescaledb-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from apiclient import BoletimRequestor
+from apitimescaledb import TimescaleDB
+
+import datetime
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(levelname)s - %(message)s")
+
+requestor = BoletimRequestor("http://www.esign.com.br:13887/smac")
+today = datetime.date.today()
+d_string = today.strftime("%d/%m/%Y")
+boletim = requestor.request(d_string)
+
+if boletim:
+    timescaleDB = TimescaleDB()
+    timescaleDB.init(boletim)
--- airflow/apitimescaledb.py
+++ airflow/apitimescaledb.py
@@ -1,5 +1,7 @@
-import psycopg2
+from apiclient import Boletim, Estacao
 
+import logging
+import psycopg2
 
 class TimescaleDB:
 
@@ -7,15 +9,44 @@ class TimescaleDB:
                  host="localhost",
                  port=5432,
                  username="postgres",
-                 password="S3cr3t",
+                 password="SuperSecret",
                  dbname="postgres"):
-        self.conn = psycopg2.connect(f"postgres://{username}:{password}@{host}:{port}/{dbname}?sslmode=require")
+        self.conn = psycopg2.connect(f"postgres://{username}:{password}@{host}:{port}/{dbname}")
 
-    def init_estacoes_table(self):
-        query_create_sensors_table = """C
-            REATE TABLE estacoes (
-                codigo SERIAL PRIMARY KEY,
+    def init(self, boletim: Boletim):
+        estacoes_table = EstacoesTable()
+        estacoes_table.create(self.conn)
+        estacoes_table.load(self.conn, boletim.estacoes)
+
+
+class EstacoesTable:
+
+    def __init__(self):
+        self.create_command = """
+            CREATE TABLE IF NOT EXISTS estacoes (
+                codigo VARCHAR(2) PRIMARY KEY,
                 nome VARCHAR(50),
-                location VARCHAR(50)
+                coordenadas GEOGRAPHY(POINT,4326)
             );
         """
+        self.insert_command = """
+            INSERT INTO estacoes (codigo, nome, coordenadas)
+            VALUES (%s, %s, 'SRID=4326;POINT(%s %s)')
+        """
+
+    def create(self, conn: psycopg2.connect):
+        cursor = conn.cursor()
+        cursor.execute(self.create_command)
+        conn.commit()
+        cursor.close()
+
+    def load(self, conn: psycopg2.connect, estacoes: list[Estacao]):
+        cursor = conn.cursor()
+        for estacao in estacoes:
+            try:
+                data = (estacao.codigo, estacao.nome, estacao.longitude, estacao.latitude)
+                cursor.execute(self.insert_command, data)
+            except (Exception, psycopg2.Error) as error:
+                logging.error(error.pgerror)
+        conn.commit()
+        cursor.close()

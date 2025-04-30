356758e7a1c195af1cda5da0efeb69cf673962ab
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2025-04-30 19:00:20 +0100

    azure timescaledb

--- main.tf
+++ main.tf
@@ -37,3 +37,46 @@ resource "azurerm_container_app_environment_storage" "qualidadearsmac_data_conta
   access_key                   = azurerm_storage_account.qualidadearsmac_data_storage_account.primary_access_key
   access_mode                  = "ReadOnly"
 }
+
+resource "azurerm_storage_container" "timescaledb_storage_container" {
+  name                  = "timescaledb"
+  storage_account_id    = azurerm_storage_account.qualidadearsmac_data_storage_account.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "timescaledb_storage_blob" {
+  for_each               = fileset("${path.module}/timescaledb", "**/*")
+  name                   = each.key
+  storage_account_name   = azurerm_storage_account.qualidadearsmac_data_storage_account.name
+  storage_container_name = azurerm_storage_container.timescaledb_storage_container.name
+  type                   = "Block"
+  source                 = "${path.module}/timescaledb/${each.key}"
+}
+
+resource "azurerm_container_app" "timescaledb_container_app" {
+  name                         = "timescaledb"
+  container_app_environment_id = azurerm_container_app_environment.qualidadearsmac_container_app_environment.id
+  resource_group_name          = azurerm_resource_group.qualidadearsmac_data_resource_group.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "timescaledb"
+      image  = "timescale/timescaledb-ha:pg17.2-ts2.18.0-all"
+      cpu    = 0.25
+      memory = "0.5Gi"
+      env {
+        name  = "POSTGRES_PASSWORD"
+        value = "SuperSecret"
+      }
+    }
+  }
+
+  ingress {
+    target_port = 5432
+    traffic_weight {
+      latest_revision = true
+      percentage = 100
+    }
+  }
+}
